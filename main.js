d5af966cc946ef49a19688368afe95dd386a8ad1
Stanley Reynara <130033729+stanReynara@users.noreply.github.com>
2024-04-18 08:57:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,66 +3,61 @@ const discordID = "YOUR DISCORD ID HERE";
 const discordWebhook = "https://discordapp.com/api/webhooks/(INSERT WEBHOOK HERE)";
 
 
-async function main(){
-    const data = fetchResults();
+async function main() {
+  const data = await fetchResults();
 
-    notification = writeNotification(data)
+  const notification = writeNotification(data);
 
-    postWebhook(notification)
+  postWebhook(notification);
 }
 
+async function fetchResults() {
+  const urls = [
+    'https://helldivers-2-dotnet.fly.dev/api/v1/assignments',
+    'https://helldivers-2-dotnet.fly.dev/api/v1/campaigns'
+  ];
 
-function fetchResults() {
-    let urls = ['https://helldivers-2-dotnet.fly.dev/api/v1/assignments','https://helldivers-2-dotnet.fly.dev/api/v1/campaigns'];
-
-    let responses = urls.map(url => UrlFetchApp.fetch(url));
-    let data = [];
-
-    for (let i = 0; i < responses.length; i++) {
-        let response = responses[i];
-        let json = JSON.parse(response.getContentText());
-        data.push(json);
-    }
-    return data
+  const responses = await Promise.all(urls.map(url => UrlFetchApp.fetch(url)));
+  return responses.map(response => JSON.parse(response.getContentText()));
 }
 
-function writeNotification(data){
-    briefingDesc = data[0][0]['briefing']
-    rewardMedals = data[0][0]['reward']['amount']
+function writeNotification(data) {
+  const { briefing, reward, expiration } = data[0][0];
+  const rewardMedals = reward.amount;
+  const formattedExpiration = Math.floor(new Date(expiration).getTime() / 1000).toString();
 
-    let response = `# MAJOR ORDER\n## ${briefingDesc}\n### Reward for Completion: ${rewardMedals} Medals.\nAvailable Planets :`
+  let response = `# MAJOR ORDER\n## ${briefing} It ends in <t:${formattedExpiration}:R> \n### Reward for Completion: ${rewardMedals} Medals.\nAvailable Planets :`;
 
-    availablePlanets = []
-    for(let i=0;i<data[1].length;i++){
-        let obj = [data[1][i]['planet']['name'],data[1][i]['planet']['currentOwner']]
-        if(obj[1] == "Automaton"){
-            availablePlanets.unshift(`\n- ${obj[0]} being controlled by ${obj[1]}.`)
-        }else{
-            availablePlanets.push(`\n- ${obj[0]} being controlled by ${obj[1]}.`)
-        }
+  availablePlanets = []
+  for(let i=0;i<data[1].length;i++){
+    let obj = [data[1][i]['planet']['name'],data[1][i]['planet']['currentOwner']]
+    if(obj[1] == "Automaton"){
+      availablePlanets.unshift(`\n- ${obj[0]} being controlled by ${obj[1]}.`)
+    }else{
+      availablePlanets.push(`\n- ${obj[0]} being controlled by ${obj[1]}.`)
     }
+  }
 
-    for(let i=0;i<availablePlanets.length;i++){
-        response += availablePlanets[i]
-    }
-    return response
+  for(let i=0;i<availablePlanets.length;i++){
+    response += availablePlanets[i]
+  }
 
+  return response;
 }
 
-    function postWebhook(data) {
+function postWebhook(data) {
+  const payload = JSON.stringify({
+    'username': 'Helldivers Alert',
+    'avatar_url': 'https://pbs.twimg.com/profile_images/1661464871777517569/ehRFQcg1_400x400.jpg',
+    'content': data
+  });
 
-    let payload = JSON.stringify({
-        'username': 'Helldivers Alert',
-        'avatar_url': 'https://pbs.twimg.com/profile_images/1661464871777517569/ehRFQcg1_400x400.jpg',
-        'content': data
-    });
+  const options = {
+    method: 'POST',
+    contentType: 'application/json',
+    payload: payload,
+    muteHttpExceptions: true
+  };
 
-    const options = {
-        method: 'POST',
-        contentType: 'application/json',
-        payload: payload,
-        muteHttpExceptions: true
-    };
-
-    UrlFetchApp.fetch(discordWebhook, options);
+  UrlFetchApp.fetch(discordWebhook, options);
 }
